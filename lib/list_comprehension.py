cd13325343f8ce4a8db4e42aeae09a6488dd4e6d
Mark <ajineer@outlook.com>
2023-07-22 12:00:59 -0600

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [num_list[n] for n in range(len(num_list)) if num_list[n]%2 == 0]
+
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence_list[n]+"!" for n in range(len(sentence_list)) if len(sentence_list) != 0]
